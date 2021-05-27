bd2c7921e7f510cd01ba4133824a67853ec5797d
rodrigo.cabral <rodrigo.cabral@championx.com>
2021-05-27 11:27:06 -0300

    amazon usando o utils

--- features/Page/amazon_page.py
+++ features/Page/amazon_page.py
@@ -1,21 +1,26 @@
 import asyncio
 from playwright.sync_api import sync_playwright
+from features.Utils.Utils import Utils
 from features.Page.elements_page.amazon_elements import Amazon_Locations
+
 
 class AmazonPage:
     def __init__(self, Page):
         self.Page = Page
+        self.UT = Utils(self.Page)
 
     def go_to_page(self):
-        self.Page.click(Amazon_Locations().LINK_AMAZON)
+        elemento = self.UT.esperar_elemento_presente(Amazon_Locations().LINK_AMAZON)
+        elemento.click()
 
     def search_Amazon(self, name):
-        self.Page.fill(Amazon_Locations().CAIXA_PESQUISA, name)
-        self.Page.press(Amazon_Locations().CAIXA_PESQUISA, 'Enter')
+        elemento = self.UT.esperar_elemento_presente(Amazon_Locations().CAIXA_PESQUISA)
+        elemento.fill(name)
+        elemento.press('Enter')
 
-    def Validate_search(self,text):
-        answer = self.Page.text_content(Amazon_Locations().PESQUISA_RESULTADO, timeout=10000)
-        assert answer == f'"{text}"'
+    def Validate_search(self, text):
+        answer = self.UT.esperar_elemento_presente(Amazon_Locations().PESQUISA_RESULTADO)
+        assert answer.text_content() == f'"{text}"'
 
     def screenshot(self, name):
         self.Page.screenshot(path=name, full_page=True)
